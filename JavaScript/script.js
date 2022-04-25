53931131c120eae4d679282134f4923e5d65ef9d
roey1234r <roey1234r@gmail.com>
2022-04-25 23:07:10 +0200

    SIS

---
+++ JavaScript/script.js
@@ -0,0 +1,212 @@
+let SideNav = document.querySelector(".SideNav");
+if (SideNav) {
+  Navigation.addEventListener("click", openNavBar);
+  exitNav.addEventListener("click", openNavBar);
+}
+
+function openNavBar(e) {
+  SideNav.classList.toggle("active");
+}
+
+function check(form) {
+  if (form.usrnm.value == "Yonit" && form.psw.value == "yonit123") {
+    // window.location.replace("index.html");
+    window.open("index.html");
+    window.close("Login_Page.html");
+  } else {
+    alert("Error Password or Username");
+    window.location.reload();
+  }
+}
+
+function Search() {
+  let input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+let Swimmers = [
+  {
+    name: "Adele",
+    age: 24,
+    city: "Haifa",
+    status: "Beginner",
+    accomplishments: "none"
+  },
+  {
+    name: "Agnes",
+    age: 18,
+    city: "Tel-Aviv",
+    status: "Beginner",
+    accomplishments: "none"
+  },
+  {
+    name: "Billy",
+    age: 26,
+    city: "Petah Tikva",
+    status: "professional",
+    accomplishments: "Golden medal"
+  },
+  {
+    name: "Bob",
+    age: 23,
+    city: "Ashdod",
+    status: "advanced",
+    accomplishments: "National medal"
+  },
+  {
+    name: "Calvin",
+    age: 20,
+    city: "Haifa",
+    status: "Beginner",
+    accomplishments: "none"
+  },
+  {
+    name: "Christina",
+    age: 20,
+    city: "Nesher",
+    status: "Beginner",
+    accomplishments: "none"
+  },
+];
+
+
+
+
+
+const add = (ev) => {
+  ev.preventDefault();
+  let dataFromLocalStorage = JSON.parse(localStorage.getItem("MySwimmerList"));
+  let Swimmer = {
+    id: 1,
+    name: document.getElementById('name').value,
+    age: document.getElementById('Age').value,
+    city: document.getElementById('city').value,
+    status: document.getElementById('status').value,
+    accomplishments: document.getElementById('accomplishments').value,
+  }
+  if (dataFromLocalStorage) {
+    dataFromLocalStorage.push(Swimmer);
+    document.forms[0].reset();
+
+    localStorage.setItem('MySwimmerList', JSON.stringify(dataFromLocalStorage).split("},{").join("},\n{"));
+    window.location.reload(false);
+  } else {
+    let Arr = [];
+    Arr.push(Swimmer);
+    localStorage.setItem('MySwimmerList', JSON.stringify(Arr).split("},{").join("},\n{"));
+    window.location.reload(false);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('AddSwimmer').addEventListener('click', add);
+})
+
+// document.addEventListener('DOMContentLoaded', () => {
+  //   document.getElementById('Delete').addEventListener('click', deleteItem);
+  // });
+  
+  let list = document.getElementById("myUL");
+  let data = JSON.parse(localStorage.getItem('MySwimmerList'));
+  data.map((item ,index) => {
+    let li;
+    // let a = document.createElement("a");
+    // let h6 = document.createElement("h6");
+    li = `<li class="liObject" data-item=${index}><span data-item=${index} class='Delete' id='Delete'>Delete</span><a href="swimmer.html"
+    <h6 data-item=${index} class='Description'>` + item.name + `Status: ` + item.status + ` Age: ` + item.age + `</h6><a></li>`;
+    // h6 = `<h6 data-item=${index} class='Description'>Status: ` + item.status + ` Age: ` + item.age + `</h6>`;
+    // li.innerHTML = item.name;
+    // li = <li data-item={index}></li>
+    // a.innerHTML = item.name + h6;
+    // li.className = "liObject";
+    // li.innerHTML = `<span data-item=${index} class='Delete' id='Delete'>Delete</span>` + "  " + a;
+    // a.href = "swimmer.html";
+    // li.appendChild(a);
+    list.innerHTML = li;
+  })
+  const deleteItem = (event)=>{
+    let allLiObject = document.querySelectorAll(".liObject");
+    
+    let newArr = [];
+    // let data = JSON.parse(localStorage.getItem('MySwimmerList'));
+    
+    for (let index = 0; index < allLiObject.length; index++) {
+      console.log("event",event.target.dataset.item);
+      console.log("li",allLiObject[index].dataset.item);
+      if(allLiObject[index].dataset.item != event.target.dataset.item){
+        newArr.push(allLiObject[index]);
+      }
+      
+    }
+    // newArr = allLiObject.filter(li => li.dataset != event.target.dataset)
+  
+      localStorage.setItem('MySwimmerList', JSON.stringify(newArr));
+      window.location.reload();
+  }
+  
+  
+let deletearr = document.querySelectorAll(".Delete")
+
+for (let i = 0; i < deletearr.length; i++) {
+  
+  deletearr[i].addEventListener("click", deleteItem);
+}
+
+// let del = document.querySelector(".Delete");
+// del.addEventListener("click", function (event) {
+//   let newArr = [];
+//   console.log(event.target);
+//   for (let index = 0; index < allLiObject.length; index++) {
+//         if(!(allLiObject[index] == event.target)){
+//             newArr.push(allLiObject[index])
+//         }
+//       }
+//       // this.closest(".liObject").remove();
+//       // allLiObject = allLiObject.filter((obj)=> obj == allLiObject[index]);
+//       // allLiObject.splice(allLiObject[index], 1);
+//       // localStorage.setItem('MySwimmerList', JSON.stringify(allLiObject));
+//     })
+
+
+
+
+function HideDeleteBtn() {
+  for (let show of document.querySelectorAll('.Delete'))
+    if (show.style.display === "block") {
+      show.style.display = "none";
+    }
+  else {
+    show.style.display = "block";
+  }
+}
+
+function openForm() {
+  document.getElementById("popupForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("popupForm").style.display = "none";
+}
+
+
+// let Place = document.querySelector(".swimmerNameTitle");
+// if (data) {
+//   data.map((item) => {
+//     let h2 = document.createElement("h2");
+//     h2.innerHTML = item.name;
+//     Place.appendChild(h2);
+//   })
+// }
